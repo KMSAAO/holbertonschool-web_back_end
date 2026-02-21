fd5630d303e85005a96e2a6c8a0ea7f67c669880
Khalid alomari <khalidalomari199@gmail.com>
2026-02-21 14:45:07 +0300

    add the documents to the file

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-
+""""
+This module contains a function that adds two floats and returns the result.
+"""
 # This is a function sum a with b and return the result
 def add(a: float, b: float) -> float:
+    """Add two floats and return the result."""
     return a + b
