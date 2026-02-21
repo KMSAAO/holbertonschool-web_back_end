8481763f8a08ee9ce2f85bc34ebfacaecc5e658d
Khalid alomari <khalidalomari199@gmail.com>
2026-02-21 23:22:15 +0300

    add

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module that defines an asynchronous generator."""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Yield 10 random floats between 0 and 10 with 1 second delay."""
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
