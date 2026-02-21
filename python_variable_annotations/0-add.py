7fac9d8dc1adc95a052d07a02b08d6085e8d6f1c
Khalid alomari <khalidalomari199@gmail.com>
2026-02-21 14:47:03 +0300

    add two lines between the doucmunts and the function

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,6 +2,8 @@
 """"
 This module contains a function that adds two floats and returns the result.
 """
+
+
 # This is a function sum a with b and return the result
 def add(a: float, b: float) -> float:
     """Add two floats and return the result."""
